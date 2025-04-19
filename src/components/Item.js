ee4c8389a92b021b40b9eb2403885f44d9ffe247
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-04-20 00:49:13 +0300

    updating state based on events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+const [isInCart, setIsInCart] = useState(false)
+
+function handleAddtoCartClick () {
+  setIsInCart((prevMode) => !prevMode)
+}
+
+const itemclass = isInCart ? "in-cart" : "";
+const buttonText = isInCart ? "Remove from Cart" : "Add to Cart";
+const buttonClass = isInCart ? "remove" : "add";
   return (
-    <li className="">
+    <li className={itemclass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={handleAddtoCartClick}>{buttonText}</button>
     </li>
   );
 }
